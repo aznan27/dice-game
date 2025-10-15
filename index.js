dcf4c409f66ac2e71bc153929c7a8a534e75c800
Aznan <aznan9686@gmail.com>
2025-10-15 21:56:54 +0530

    added buttons

--- index.js
+++ index.js
@@ -1,19 +1,50 @@
-var diceOne = Math.random();
-diceOne = diceOne * 6;
-diceOne = Math.floor(diceOne) + 1;
-console.log(diceOne);
+let diceOne;
+let diceTwo;
+let result = document.querySelector("h1");
+let btnOne = document.querySelector(".btn-one");
+let btnTwo = document.querySelector(".btn-two");
+let refreshBtn = document.querySelector(".refresh");
+let imgDiceOne = document.querySelector(".img1");
+let imgDiceTwo = document.querySelector(".img2");
 
-var diceTwo = Math.random();
-diceTwo = diceTwo * 6;
-diceTwo = Math.floor(diceTwo) + 1;
-console.log(diceTwo);
-
-if (diceOne > diceTwo) {
-    console.log(document.querySelector("h1").innerHTML = "Player 1 Wins!");
+function refresh() {
+    diceOne = "";
+    diceTwo = "";
+    imgDiceOne.setAttribute("src", "images/dice6.png");
+    imgDiceTwo.setAttribute("src", "images/dice6.png");
+    result.innerHTML = "Let's Play";
+    btnOne.removeAttribute("disabled");
+    btnTwo.removeAttribute("disabled");
 }
-if (diceTwo > diceOne) {
-    console.log(document.querySelector("h1").innerHTML = "Player 2 Wins!");
-} else if (diceOne === diceTwo) {
-    console.log(document.querySelector("h1").innerHTML = "It's A Draw!");
+
+function rollDice(player) {
+    if (player === "one") {
+        diceOne = Math.random();
+        diceOne = diceOne * 6;
+        diceOne = Math.floor(diceOne) + 1;
+        let diceImage1 = "images/dice" + diceOne + ".png";
+        imgDiceOne.setAttribute("src", diceImage1);
+        btnOne.setAttribute("disabled", "true");
+    } else {
+        diceTwo = Math.random();
+        diceTwo = diceTwo * 6;
+        diceTwo = Math.floor(diceTwo) + 1;
+        let diceImage2 = "images/dice" + diceTwo + ".png";
+        imgDiceTwo.setAttribute("src", diceImage2);
+        btnTwo.setAttribute("disabled", "true");
+    }
+
+    if (diceOne && diceTwo) {
+        if (diceOne > diceTwo) {
+            console.log((result.innerHTML = "Player 1 Wins!"));
+        } else if (diceTwo > diceOne) {
+            console.log((result.innerHTML = "Player 2 Wins!"));
+        } else {
+            console.log((result.innerHTML = "Draw!"));
+        }
+    }
 }
 
+btnOne.addEventListener("click", () => rollDice("one"));
+btnTwo.addEventListener("click", () => rollDice("two"));
+refreshBtn.addEventListener("click", refresh);
