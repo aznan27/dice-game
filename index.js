30d23aeef66892c9e6813a0e1949e4129f868f69
Aznan <aznan9686@gmail.com>
2025-10-15 21:51:47 +0530

    number generation and winner declare

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-console.log("hello world");
+var diceOne = Math.random();
+diceOne = diceOne * 6;
+diceOne = Math.floor(diceOne) + 1;
+console.log(diceOne);
+
+var diceTwo = Math.random();
+diceTwo = diceTwo * 6;
+diceTwo = Math.floor(diceTwo) + 1;
+console.log(diceTwo);
+
+if (diceOne > diceTwo) {
+    console.log(document.querySelector("h1").innerHTML = "Player 1 Wins!");
+}
+if (diceTwo > diceOne) {
+    console.log(document.querySelector("h1").innerHTML = "Player 2 Wins!");
+} else if (diceOne === diceTwo) {
+    console.log(document.querySelector("h1").innerHTML = "It's A Draw!");
+}
 
